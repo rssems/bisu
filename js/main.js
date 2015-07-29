03f854e80dc83ab6077f32cc9cfe50acfb1b0bbf
Riza Selcuk Saydam <rss.ems@gmail.com>
2015-07-29 17:14:02 +0300

    js a little

--- js/main.js
+++ js/main.js
@@ -28,3 +28,25 @@ $document.scroll(function() {
     $element.removeClass(className);
   }
 });
+
+// setTimeout(function() {
+// 	$('.app_preview').addClass('preview1');
+// }, 3000);
+
+function removeC() {
+	$('.app_preview').removeClass('preview1');
+	$('.app_preview').removeClass('preview2');
+	$('.app_preview').removeClass('preview3');
+}
+$( "#ssc1" ).click(function() {
+	removeC();
+	$('.app_preview').addClass('preview1');
+});
+$( "#ssc2" ).click(function() {
+	removeC();
+	$('.app_preview').addClass('preview2');
+});
+$( "#ssc3" ).click(function() {
+	removeC();
+	$('.app_preview').addClass('preview3');
+});
